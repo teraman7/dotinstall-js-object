fcb8f87ddb67103fac6b880828f8ba61fc2d30ac
teraman7 <puman0815@gmail.com>
2021-11-21 18:15:02 +0900

    lesson 2021-11-21

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,8 +1,42 @@
 'use strict';
 
 {
-  // console.log(Math.random());//ランダム値（0 ~ 1）
+  class Post { // 親クラス
+    constructor(text){
+      this.text = text,
+      this.likeCount = 0
+    }
 
-  console.log(Math.floor(Math.random() * 6) + 1);
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
 
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+
+  }
+
+  class SponsoredPost extends Post { // 子クラス
+    constructor(text, sponsor){
+      super(text);
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      super.show();
+      console.log(`...sponsored by ${this.sponsor}`);
+    }
+
+  }
+
+  const posts = [
+    new Post('JavaScript'),
+    new Post('Python'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall')
+  ];
+
+  posts[2].show();
+  posts[2].like();
 }
